20c2fbab4918efe5524891b89c5e5e6065825a41
Patrick Traynor <patrickjtraynor@gmail.com>
2013-01-20 20:07:19 -0800

    Initial commit
    (About 2 weekends of work)
    Features in this commit:
    - basic structure of editing canvas with gridlines and zoom
    - temporary color selector
    - 3 tools: pencil, fill, and line

---
+++ TestApp2/GridOverlay.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Media;
+
+namespace TestApp2
+{
+    /// <summary>
+    /// Displays and modifies the grid overlay.
+    /// </summary>
+    class GridOverlay
+    {
+        Line[] HLines;
+        Line[] VLines;
+        Line[] BorderLines;
+        Rectangle SelectRect1;
+        Rectangle SelectRect2;
+
+        SolidColorBrush GridBrush;
+        SolidColorBrush GridBrushBorder;
+        SolidColorBrush GridBrushSelect;
+
+        PixelDisplay Display;
+
+        /// <summary>
+        /// Creates a new GridOverlay.
+        /// </summary>
+        public GridOverlay(PixelDisplay d)
+        {
+            Display = d;
+            GridBrush = new SolidColorBrush(Colors.LightGray);
+            GridBrushBorder = new SolidColorBrush(Colors.Gray);
+            GridBrushSelect = new SolidColorBrush(Colors.DimGray);
+            HLines = new Line[15];
+            VLines = new Line[15];
+            BorderLines = new Line[4];
+            for (int i = 0; i < HLines.Length; i++) HLines[i] = new Line() { Stroke = GridBrush };
+            for (int i = 0; i < VLines.Length; i++) VLines[i] = new Line() { Stroke = GridBrush };
+            for (int i = 0; i < BorderLines.Length; i++) BorderLines[i] = new Line() { Stroke = GridBrushBorder };
+            SelectRect1 = new Rectangle() { Stroke = GridBrushSelect };
+            SelectRect2 = new Rectangle() { Stroke = GridBrushSelect };
+        }
+
+
+        /// <summary>
+        /// Updates the position of the grid on the canvas.
+        /// </summary>
+        public void UpdateDisplay()
+        {
+            int gridWidth = Display.GetPixelsWidth() * Display.PixelSize;
+            int gridHeight = Display.GetPixelsHeight() * Display.PixelSize;
+            for (int i = 0; i < HLines.Length; i++)
+            {
+                Line line = HLines[i];
+                Display.PixelCanvas.Children.Remove(line);
+                line.X1 = Display.XOffset;
+                line.X2 = Display.XOffset + gridWidth;
+                line.Y1 = line.Y2 = Display.YOffset + (i + 1) * Display.PixelSize;
+                if (Display.GridEnabled) Display.PixelCanvas.Children.Add(line);
+            }
+            for (int i = 0; i < VLines.Length; i++)
+            {
+                Line line = VLines[i];
+                Display.PixelCanvas.Children.Remove(line);
+                line.Y1 = Display.YOffset;
+                line.Y2 = Display.YOffset + gridHeight;
+                line.X1 = line.X2 = Display.XOffset + (i + 1) * Display.PixelSize;
+                if (Display.GridEnabled) Display.PixelCanvas.Children.Add(line);
+            }
+            for (int i = 0; i < BorderLines.Length; i++)
+            {
+                Line line = BorderLines[i];
+                Display.PixelCanvas.Children.Remove(line);
+                switch (i)
+                {
+                    // Vertical lines
+                    case 0: line.Y1 = Display.YOffset; line.Y2 = Display.YOffset + gridHeight; line.X1 = line.X2 = Display.XOffset; break;
+                    case 1: line.Y1 = Display.YOffset; line.Y2 = Display.YOffset + gridHeight; line.X1 = line.X2 = Display.XOffset + gridWidth; break;
+                    // Horizontal lines
+                    case 2: line.X1 = Display.XOffset; line.X2 = Display.XOffset + gridWidth; line.Y1 = line.Y2 = Display.YOffset; break;
+                    case 3: line.X1 = Display.XOffset; line.X2 = Display.XOffset + gridWidth; line.Y1 = line.Y2 = Display.YOffset + gridHeight; break;
+                }
+                if (Display.BorderEnabled) Display.PixelCanvas.Children.Add(line);
+            }
+
+            Display.PixelCanvas.Children.Remove(SelectRect1);
+            Display.PixelCanvas.Children.Remove(SelectRect2);
+            if (Display.Select1Enabled)
+            {
+                
+                SelectRect1.Width = SelectRect1.Height = Display.PixelSize;
+                Canvas.SetLeft(SelectRect1, 0);
+                Canvas.SetTop(SelectRect1, 0);
+                Display.PixelCanvas.Children.Add(SelectRect1);
+            }
+            if (Display.Select2Enabled)
+            {
+
+            }
+        }
+
+        public void SetRect1Position(int x, int y)
+        {
+
+        }
+
+
+    }
+}
---
+++ TestApp2/MainPage.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.System;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TestApp2
+{
+    /// <summary>
+    /// The main page of the application. This stores several "global" application values.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        PixelDisplay PixDisplay;
+        ColorPicker Picker;
+        Boolean CtrlDown;
+
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+            Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
+            Window.Current.CoreWindow.PointerPressed += CoreWindow_PointerPressed;
+            Window.Current.CoreWindow.PointerReleased += CoreWindow_PointerReleased;
+            Window.Current.CoreWindow.PointerMoved += CoreWindow_PointerMoved;
+            Window.Current.CoreWindow.PointerWheelChanged += CoreWindow_PointerWheelChanged;
+            
+
+            PixDisplay = new PixelDisplay(pixelCanvas, debugText);
+            Picker = new ColorPicker(new Slider[] { colorSlider1, colorSlider2, colorSlider3 }, pickerRectangle);
+
+            // Set values now since PixDisplay isn't null.
+            zoomSlider.Maximum = 10000;
+            zoomSlider.Minimum = 100;
+            zoomSlider.Value = 1000;
+            zoomSlider.SmallChange = 100;
+            zoomSlider.LargeChange = 100;
+            zoomSlider.StepFrequency = 100;
+            borderCheckBox.IsChecked = true;
+        }
+
+        // EVENT HANDLERS //
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        void CoreWindow_PointerMoved(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.PointerEventArgs args)
+        {
+            int x = (int)args.CurrentPoint.Position.X;
+            int y = (int)args.CurrentPoint.Position.Y;
+            PixDisplay.PointerMoved(x, y);
+        }
+
+        void CoreWindow_PointerPressed(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.PointerEventArgs args)
+        {
+            int x = (int)args.CurrentPoint.Position.X;
+            int y = (int)args.CurrentPoint.Position.Y;
+            PixDisplay.PointerChanged(x, y, true);
+        }
+
+        void CoreWindow_PointerReleased(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.PointerEventArgs args)
+        {
+            int x = (int)args.CurrentPoint.Position.X;
+            int y = (int)args.CurrentPoint.Position.Y;
+            PixDisplay.PointerChanged(x, y, false);
+        }
+        
+        void CoreWindow_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            if (args.VirtualKey == VirtualKey.Control) CtrlDown = true;
+            if (CtrlDown)
+            {
+                if (args.VirtualKey == VirtualKey.Z) PixDisplay.Undo();
+                if (args.VirtualKey == VirtualKey.Y) PixDisplay.Redo();
+            }
+
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Number1: radio1.IsChecked = true; break;
+                case VirtualKey.Number2: radio2.IsChecked = true; break;
+                case VirtualKey.Number3: radio3.IsChecked = true; break;
+            }
+        }
+
+        void CoreWindow_KeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            if (args.VirtualKey == VirtualKey.Control) CtrlDown = false;
+        }
+
+        void CoreWindow_PointerWheelChanged(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.PointerEventArgs args)
+        {
+            if (args.CurrentPoint.Properties.MouseWheelDelta == -120) zoomSlider.Value -= 100;
+            if (args.CurrentPoint.Properties.MouseWheelDelta == 120) zoomSlider.Value += 100;
+        }
+        
+        private void ZoomSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            PixDisplay.SetPixelSize((int)zoomSlider.Value / 100);
+        }
+
+        private void GridCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            PixDisplay.SetGridEnabled(true);
+        }
+
+        private void GridCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PixDisplay.SetGridEnabled(false);
+        }
+
+        private void Radio1_Checked(object sender, RoutedEventArgs e)
+        {
+            if (PixDisplay != null) PixDisplay.SetDrawTool(PixelDisplay.DrawTool.Pencil);
+        }
+
+        private void Radio2_Checked(object sender, RoutedEventArgs e)
+        {
+            PixDisplay.SetDrawTool(PixelDisplay.DrawTool.FillBucket);
+        }
+
+        private void Radio3_Checked(object sender, RoutedEventArgs e)
+        {
+            PixDisplay.SetDrawTool(PixelDisplay.DrawTool.Line);
+        }
+
+        private void BorderCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            PixDisplay.SetBorderEnabled(true);
+        }
+
+        private void BorderCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PixDisplay.SetBorderEnabled(false);
+        }
+
+        private void SaveButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            //PixIO.Save(PixDisplay.GetCurrentColors());
+        }
+        
+    }
+}
